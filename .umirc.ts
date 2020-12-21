3bc385912e83be9df3405a5240121ea7dbb83925
whatwewant <tobewhatwewant@gmail.com>
2020-12-21 11:44:25 +0800

    chore: fix github repo info

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,6 @@
 export default {
-  base: '/github-favorites',
-  publicPath: '/github-favorites/',
+  base: '/webafe/BAFE-Weekly',
+  publicPath: '/webafe/BAFE-Weekly/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   // 其他配置
   dynamicImport: {},
