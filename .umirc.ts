fba14aee581fb5cfbedb25ddae4ff9a83ccf4063
whatwewant <tobewhatwewant@gmail.com>
2020-12-22 10:55:53 +0800

    chore: update umirc for domain

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,6 @@
 export default {
-  base: '/BAFE-Weekly',
-  publicPath: '/BAFE-Weekly/',
+  base: '/',
+  publicPath: '/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   // 其他配置
   dynamicImport: {},
