5876d11f8cf2f3a5795eb2bdb6683533a14963d2
whatwewant <tobewhatwewant@gmail.com>
2020-12-22 16:22:00 +0800

    chore: add GA

--- .umirc.ts
+++ .umirc.ts
@@ -4,4 +4,8 @@ export default {
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   // 其他配置
   dynamicImport: {},
+  scripts: [
+    ';window._Z_GA_ID="G-7FHN9TL66S"',
+    '!function(e){function a(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],a("js",new Date),a("config",e);var t=document.createElement("script");t.async=!0,t.src="https://www.googletagmanager.com/gtag/js?id="+e;var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n)}(window._Z_GA_ID);',
+  ],
 };
